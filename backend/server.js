efee6c46acb6cc2cb06dad0c109ef2d4c754429e
jash-art <jeswanthniko@gmail.com>
2025-05-17 18:22:51 +0530

    IC3

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import morgan from 'morgan'
+import path from 'path'
 import { connectDB } from './src/services/db/index.js'
 import urlRoutes from './src/routes/urlRoutes.js'
 
